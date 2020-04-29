e6504f8e8540daedc27edff2486210f0b646e52f
Ruidong Wu <mooninrain@outlook.com>
2020-04-29 17:45:11 -0400

    Update utils.py

--- image_generation/utils.py
+++ image_generation/utils.py
@@ -96,7 +96,7 @@ def add_object(object_dir, name, scale, loc, theta=0):
 
   # import partnet parts
   imported_object = bpy.ops.import_scene.obj(filepath='/data/vision/billf/scratch/ruidongwu/data/data_v0/10158/objs/new-4.obj')
-  name = 'new-0'
+  name = 'new-4'
   import pdb; pdb.set_trace()
 
   # Give it a new name to avoid conflicts
